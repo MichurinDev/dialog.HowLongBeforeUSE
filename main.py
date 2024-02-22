3cc3f244e92885eae7c546c54104792a8eda37f9
MichurinDev <andreymithurin00@gmail.com>
2024-02-23 02:35:25 +0700

    Фикс багов + подсчёт количества дней

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ month = {
 def handler(event, context):
     text = 'Привет! Задаёшься вопросом, сколько осталось до ЕГЭ? Сейчас выясним! По какому предмету хочешь узнать дату?'
 
-    with open("dates.json") as f:
+    with open("dates.json", encoding="utf-8") as f:
         dates = json.loads(f.read())
 
     if 'request' in event and 'original_utterance' in event['request'] and len(event['request']['original_utterance']) > 0:
         exam_dates = dates.get(event['request']['original_utterance'].lower(), "Такой предмет отсутствует!")
-        print(exam_dates)
+
         if exam_dates != "Такой предмет отсутствует!":
             main_dates = exam_dates["main"]
             reserve_dates = exam_dates["reserve"]
 
+            exam_day = datetime.datetime.strptime(main_dates[0], "%d.%m")
+            exam_day = datetime.date(year=2024, month=exam_day.month, day=exam_day.day)
+            delta = str(exam_day - datetime.date.today()).split()[0]
+            text = f"Осталось {delta} дней!"
+                
             temp_arr = []
-            text = "Основной период: "
+            text += "\nОсновной период: "
             for d in main_dates:
                 day, mon = d.split(".")
                 temp_arr.append(f"{day} {month[mon]}")
